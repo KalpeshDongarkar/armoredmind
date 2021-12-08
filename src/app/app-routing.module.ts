008dc96c478e06e189b957918c537af511593909
KalpeshDongarkar <kalpeshdongarkar67@gmail.com>
2021-12-08 22:43:49 +0530

    firstcomit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './accAuth/login/login.component';
+import { RegComponent } from './accAuth/reg/reg.component';
+import { FAQComponent } from './helpUs/faq/faq.component';
+import { AboutUsComponent } from './mainmenu/about-us/about-us.component';
+import { ContactUsComponent } from './mainmenu/contact-us/contact-us.component';
+import { HomeComponent } from './mainmenu/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegComponent },
+  { path: 'contactus', component: ContactUsComponent },
+  { path: 'faq', component: FAQComponent },
+  { path: 'aboutus', component: AboutUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './accAuth/login/login.component';
+import { RegComponent } from './accAuth/reg/reg.component';
+import { ContactUsComponent } from './mainmenu/contact-us/contact-us.component';
+import { FAQComponent } from './helpUs/faq/faq.component';
+import { AboutUsComponent } from './mainmenu/about-us/about-us.component';
+import { HomeComponent } from './mainmenu/home/home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegComponent,
+    ContactUsComponent,
+    FAQComponent,
+    AboutUsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
